12acf23aab4ef9c0de750e4eaaa6899cc6fb7a0e
Willan Diaz <wadiaz@uce.edu.ec>
2023-02-28 19:43:03 -0500

    Taller 36: Controller

---
+++ src/main/java/com/example/demo/ProyectoPaU4Wd1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU4Wd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU4Wd1Application.class, args);
+	}
+
+}
